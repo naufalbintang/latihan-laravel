cbd55b90f630130712e60e4ffa7b06ebc2af68dc
naufalbintang <naufalbintangpradana@gmail.com>
2026-01-20 17:22:29 +0700

    Menambahkan Factory dan mengaktifkan fitur HasFactory pada Model
    1. Menambahkan trait HasFactory pada model Postdan Team untuk mendukung pembuatan data dummy.
    2. Membuat file PostFactory untuk mendefinisikan skema data dummy artikel blog.
    3. Membuat file TeamFactory untuk mendefinisikan skema data dummy anggota tim.

---
+++ database/factories/TeamFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Team>
+ */
+class TeamFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'role' => fake()->randomElement(['Backend Developer', 'Frontend Developer', 'UI/UX Developer']),
+            'medsoc' => fake()->name()
+        ];
+    }
+}
