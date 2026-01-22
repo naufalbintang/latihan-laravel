6cccc142f07c1350b4ec5b15b8d1502d0323d7ef
naufalbintang <naufalbintangpradana@gmail.com>
2026-01-22 20:51:06 +0700

    Implementasi Relasi Eloquent Antara Model Post dan User 1. Menambahkan kolom foreign key author_id pada migration tabel posts yang terhubung ke tabel users. 2. Mendefinisikan relasi author (BelongsTo) pada model Post dan relasi posts (HasMany) pada model User. 3. Memperbarui PostFactory untuk membuat data author_id menggunakan User::factory(). 4. Menambahkan route baru /authors/{user} untuk memfilter artikel berdasarkan penulis. 5. Menyesuaikan view posts.blade.php untuk menampilkan nama penulis melalui relasi author .

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Post;
 use App\Models\Team;
+use App\Models\User;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -29,6 +30,10 @@ Route::get('/posts/{post:slug}', function (Post $post) {
             'post' => $post
         ]
     );
+});
+
+Route::get('/authors/{user}', function (User $user) {
+    return view('posts', ['title' => 'Articles by ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/about', function () {
